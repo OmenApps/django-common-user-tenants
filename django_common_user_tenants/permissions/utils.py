24eff4345ebc6305da551e57d06817900a5d635d
watervize <support@omenapps.com>
2019-05-27 07:36:16 -0700

    Updated to check for authentication first

--- django_common_user_tenants/permissions/utils.py
+++ django_common_user_tenants/permissions/utils.py
@@ -13,6 +13,7 @@ class TenantPermissionsRequiredMixin(AccessMixin):
             if not request.user.has_tenant_permissions():
                 raise PermissionDenied
             return super().dispatch(request, *args, **kwargs)
+        return False
 
 
 def tenant_permissions_required(function):
@@ -20,11 +21,13 @@ def tenant_permissions_required(function):
     Decorator for views that checks that the user has permissions on the current tenant, redirecting
     to the log-in page if necessary.
     """
-    with tenant_context(get_current_tenant()):
-        def _inner(request, *args, **kwargs):
-            if not request.user.has_tenant_permissions():
-                raise PermissionDenied           
-            return function(request, *args, **kwargs)
-        return _inner
+    if request.user.is_authenticated:
+        with tenant_context(get_current_tenant()):
+            def _inner(request, *args, **kwargs):
+                if not request.user.has_tenant_permissions():
+                    raise PermissionDenied           
+                return function(request, *args, **kwargs)
+            return _inner
+    return False
 
 
